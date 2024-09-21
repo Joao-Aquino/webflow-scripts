4f2e2e3e20b41fb4230cd99aa59632be2c0175e1
João Aquino <jaoaquino316@gmail.com>
2024-09-21 15:25:29 -0300

    geral

---
+++ geral/smooth-scrool-main.js
@@ -0,0 +1,15 @@
+data-id-scroll
+  data-autoinit="true"
+  data-duration="1"
+  data-orientation="vertical"
+  data-smoothWheel="true"
+  data-smoothTouch="false"
+  data-touchMultiplier="1.5"
+  data-easing="(t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t))"
+  data-useOverscroll="true"
+  data-useControls="true"
+  data-useAnchor="true"
+  data-useRaf="true"
+  data-infinite="false"
+  defer
+  src="geral/smooth-scrool.js"
