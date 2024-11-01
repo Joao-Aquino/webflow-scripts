ccc33c5bb0f1383bccecf6a3630a6fa46613f543
João Aquino <jaoaquino316@gmail.com>
2024-11-01 01:21:12 -0300

    splide-home-return

--- hidromar/splide--home-banner.js
+++ hidromar/splide--home-banner.js
@@ -1,12 +1,7 @@
-// Função genérica para inicializar sliders com as configurações padrão
-function initSplide(
-  selector,
-  progressBarSelector,
-  nextButtonSelector,
-  prevButtonSelector
-) {
+// Função para inicializar um carrossel
+function initSplide(selector, progressBarSelector) {
   var splide = new Splide(selector, {
-    type: "slide", // Configuração padrão para os sliders comuns
+    type: "loop",
     trimSpace: "move",
     overflow: "visible !important",
     padding: "2rem",
@@ -15,7 +10,6 @@ function initSplide(
     snap: true,
     perPage: 3,
     lazyload: "nearby",
-    autoplay: false, // Sem autoplay por padrão
     breakpoints: {
       767: {
         perPage: 2,
@@ -34,53 +28,20 @@ function initSplide(
   splide.on("mounted move", function () {
     var end = splide.Components.Controller.getEnd() + 1;
     var rate = Math.min((splide.index + 1) / end, 1);
-    if (bar) bar.style.width = String(100 * rate) + "%";
+    bar.style.width = String(100 * rate) + "%";
   });
 
-  // Monta o slider
   splide.mount();
-
-  // Configura os botões de controle individuais
-  document
-    .querySelector(nextButtonSelector)
-    .addEventListener("click", function () {
-      splide.go(">");
-    });
-
-  document
-    .querySelector(prevButtonSelector)
-    .addEventListener("click", function () {
-      splide.go("<");
-    });
 }
 
-// Inicializa sliders com configurações padrão
-initSplide(
-  "#splide-elevadores",
-  "#progresso-elevadores",
-  "#next-elevadores",
-  "#prev-elevadores"
-);
-initSplide(
-  "#splide-lavadoras",
-  "#progresso-lavadoras",
-  "#next-lavadoras",
-  "#prev-lavadoras"
-);
+// Inicializa os dois carrosséis usando a função genérica
+initSplide("#splide-elevadores", "#progresso-elevadores");
+initSplide("#splide-lavadoras", "#progresso-lavadoras");
 
-// Inicializa o #splide-banner com configurações específicas e sem herança das configurações padrão
-var splideBanner = new Splide("#splide-banner", {
-  type: "loop",
-  autoplay: true,
+// Controle dos botões de próximo e anterior
+$(".next-splide").click(function () {
+  $(".splide__arrow.splide__arrow--next").click();
 });
-
-splideBanner.mount();
-
-// Configura os botões de controle para o #splide-banner
-document.querySelector("#next-banner").addEventListener("click", function () {
-  splideBanner.go(">");
-});
-
-document.querySelector("#prev-banner").addEventListener("click", function () {
-  splideBanner.go("<");
+$(".prev-splide").click(function () {
+  $(".splide__arrow.splide__arrow--prev").click();
 });
