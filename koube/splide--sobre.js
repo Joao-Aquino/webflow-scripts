8d15964646492daade15b9a74f477abaed37e902
João Aquino <jaoaquino316@gmail.com>
2025-01-20 13:51:26 -0300

    splide-sobre-koube

---
+++ koube/splide--sobre.js
@@ -0,0 +1,71 @@
+// Função para inicializar um carrossel
+function initSplide(
+  selector,
+  progressBarSelector,
+  nextBtnSelector,
+  prevBtnSelector
+) {
+  var splide = new Splide(selector, {
+    trimSpace: "move",
+    overflow: "visible !important",
+    padding: { left: "0rem", right: "1.5rem" },
+    focus: "center",
+    omitEnd: true,
+    drag: "free",
+    snap: true,
+    perPage: 3,
+    lazyload: "nearby",
+    breakpoints: {
+      767: {
+        perPage: 2,
+        padding: { left: "0rem", right: "1.5rem" },
+      },
+      568: {
+        perPage: 2,
+        padding: { left: "0rem", right: "1.5rem" },
+      },
+    },
+  });
+
+  var bar = splide.root.querySelector(progressBarSelector);
+
+  // Atualiza a barra de progresso
+  splide.on("mounted move", function () {
+    var end = splide.Components.Controller.getEnd() + 1;
+    var rate = Math.min((splide.index + 1) / end, 1);
+    bar.style.width = String(100 * rate) + "%";
+  });
+
+  splide.mount();
+
+  // Adiciona controles aos botões específicos
+  if (nextBtnSelector) {
+    document
+      .querySelector(nextBtnSelector)
+      .addEventListener("click", function () {
+        splide.go(">");
+      });
+  }
+
+  if (prevBtnSelector) {
+    document
+      .querySelector(prevBtnSelector)
+      .addEventListener("click", function () {
+        splide.go("<");
+      });
+  }
+}
+
+// Inicializa os carrosséis de forma independente
+initSplide(
+  "#splide-conexoes",
+  "#progresso-conexoes",
+  "#next-conexoes",
+  "#prev-conexoes"
+);
+initSplide(
+  "#splide-testimonial",
+  "#progresso-testimonial",
+  "#next-testimonial",
+  "#prev-testimonial"
+);
