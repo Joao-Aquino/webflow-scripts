864f78be1824991be4b2167cb1ac22cac1110b23
João Aquino <51887070+Joao-Aquino@users.noreply.github.com>
2025-03-12 15:06:41 -0300

    Create smooth-scroll.js

---
+++ keep/smooth-scroll.js
@@ -0,0 +1,15 @@
+	data-id-scroll
+	data-autoinit="true"
+	data-duration="1"
+	data-orientation="vertical"
+	data-smoothWheel="true"
+	data-smoothTouch="false"
+	data-touchMultiplier="1.5"
+	data-easing="(t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t))"
+	data-useOverscroll="true"
+	data-useControls="true"
+	data-useAnchor="true"
+	data-useRaf="true"
+	data-infinite="false"
+	defer
+	src="https://cdn.prod.website-files.com/66b0ca6b3eb10a0f12259b2a/66d60ecfcb57c7917f4597f5_smooth-scrool-file.txt">
