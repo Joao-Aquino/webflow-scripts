341a7854f95656bab96aa7845f82220d4261b24e
João Aquino <jaoaquino316@gmail.com>
2025-03-27 11:19:43 -0300

    radio-splide-home

--- radio-educadora/splide-home.js
+++ radio-educadora/splide-home.js
@@ -6,7 +6,7 @@ function initSplide(
   prevBtnSelector
 ) {
   // Configurações padrão
-  var splide = new Splide(selector, {
+  var options = {
     trimSpace: "move",
     overflow: "visible !important",
     margin: { left: "0rem", right: "2rem" },
@@ -26,7 +26,28 @@ function initSplide(
         margin: { left: "0rem", right: "1.5rem" },
       },
     },
-  });
+  };
+
+  // Se o seletor for para o banner, define perPage para 1
+  if (selector === "#splide-banner") {
+    options.perPage = 1;
+    options.type = "loop";
+    (options.autoplay = true),
+      (options.interval = "6000"),
+      // (Opcional) Ajusta os breakpoints para o banner se necessário
+      (options.breakpoints = {
+        767: {
+          perPage: 1,
+          margin: { left: "0rem", right: "1.5rem" },
+        },
+        568: {
+          perPage: 1,
+          margin: { left: "0rem", right: "1.5rem" },
+        },
+      });
+  }
+
+  var splide = new Splide(selector, options);
 
   var bar = splide.root.querySelector(progressBarSelector);
 
@@ -59,8 +80,21 @@ function initSplide(
 
 // Inicializa os carrosséis de forma independente
 initSplide(
-  "#splide-relacionados",
-  "#progresso-relacionados",
-  "#next-relacionados",
-  "#prev-relacionados"
+  "#splide-banner",
+  "#progresso-banner",
+  "#next-banner",
+  "#prev-banner"
+);
+initSplide("#splide-manha", "#progresso-manha", "#next-manha", "#prev-manha");
+initSplide(
+  "#splide-programacao",
+  "#progresso-programacao",
+  "#next-programacao",
+  "#prev-programacao"
+);
+initSplide(
+  "#splide-noticias",
+  "#progresso-noticias",
+  "#next-noticias",
+  "#prev-noticias"
 );
